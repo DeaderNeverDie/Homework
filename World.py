d36d7ea083b458b49f8aa535ffe1bab26119b787
DeaderNeverDie <genek.3000@gmail.com>
2024-10-14 16:35:44 +0300

    Add files via upload

---
+++ World.py
@@ -0,0 +1,13 @@
+#1st program
+print("ответ 1:",(9 ** 0.5) * 5)
+#2nd program
+print("ответ 2:",9.99 > 9.98 and 1000 != 1000.1)
+#3rd program
+print(2 * 2 +2)
+print(2 * (2 +2))
+print("ответ 3:", ((2 * 2 +2) == (2 * (2 +2)) ))
+#4th program
+n = float("123.456")
+n = n * 10
+n = int(n)
+print("ответ 4:", n % 10)
