7ee929ea1684fb07a3f036340e8664cda43bdb63
DeaderNeverDie <genek.3000@gmail.com>
2024-10-27 11:56:19 +0300

    Add files via upload

---
+++ module_2_hard.py
@@ -0,0 +1,13 @@
+number = int(input('Введите число от 3 до 20: '))
+password = []
+for i in range(1, number):
+    for j in range(2, number):
+        flag = any(j == i for i in password)
+        if flag:
+            continue
+        if number % (i + j) == 0 and i != j and (i or j) <= (number / 2):
+            password.append(i)
+            password.append(j)
+        else:
+            continue
+print(*password)
